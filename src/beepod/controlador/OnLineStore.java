3ab70e96f1d89fe866ef98a83362a0cd94c05216
jlge30 <jlge30@gmail.com>
2023-03-20 21:18:54 +0100

    menús de cliente 1 y 2

--- src/beepod/controlador/OnLineStore.java
+++ src/beepod/controlador/OnLineStore.java
@@ -2,9 +2,8 @@ package beepod.controlador;
 
 
 import beepod.vista.GestionOs;
-import beepod.vista.OnlineStore;
 
-public class Main {
+public class OnLineStore {
     public static void main(String[] args) {
         GestionOs gestionOs = new GestionOs();
         gestionOs.inicio();
