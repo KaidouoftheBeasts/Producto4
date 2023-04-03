05fc9ba92f196d7378fec494906bba5470e5def9
jlge30 <jlge30@gmail.com>
2023-04-03 10:08:45 +0200

    Añadidos dos test sencillos, creando el método para que nos retorne el total del pedido

--- src/beepod/controlador/TestJunit.java
+++ src/beepod/controlador/TestJunit.java
@@ -1,21 +1,31 @@
 package beepod.controlador;
-import beepod.modelo.Pedido;
+import beepod.modelo.*;
 import junit.framework.TestCase;
 import org.junit.Test;
 
 public class TestJunit extends TestCase {
-    private Pedido pedido;
 
-
-    public void escenario (){
-        pedido = new Pedido();
+    public void TestJunit(){
     }
+
     @Test
-    public void testTotalEnvio(){
-        escenario();
-        int cantidad = 20;
-
-
+    public void testArticuloGetGastosEnvio(){
+        Articulo articulo = new Articulo("1", "Prueba Test", 20, 10, 10);
+        float resultadoEsperado = 10;
+        float resultado = articulo.getGastosEnvio();
+        assertEquals(resultadoEsperado,resultado);
+        fail("El Test es ok!!");
     }
+    //String codigo, String descripcion, float precioVenta, float gastosEnvio, long tiempoPreparacion
+    @Test
+    public void testTotalPedido(){
 
+        Articulo articulo = new Articulo("1","Es Una prueba",20,10,10 );
+        ClientePremium clientePremium = new ClientePremium("Jose","prueba","1234","email");
+        Pedido pedido = new Pedido();
+        float resultadoEsperado = 208;
+        float resultado = pedido.totalPedido(10,articulo,clientePremium);
+        assertEquals(resultadoEsperado,resultado);
+        fail("El Test del pedido es ok!!");
+    }
 }
